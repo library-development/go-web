51b450805abfe10fe77fc2692260cab474bea876
Michael Rybka <merybka@gmail.com>
2022-12-05 11:56:04 -0500

    Remove serve

--- app.go
+++ app.go
@@ -1,13 +1,5 @@
 package web
 
-import "net/http"
-
 type App struct {
 	Models []Model `json:"models"`
-}
-
-func (a *App) Serve(port, dataDir string) error {
-	mux := http.NewServeMux()
-	mux.Handle("/", &AppInstance{DataDir: dataDir, Source: a})
-	return http.ListenAndServe(":"+port, mux)
 }
