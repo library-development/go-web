ca31c7926d1638e3b6e825270025268d63dba06c
Michael Rybka <merybka@gmail.com>
2022-12-16 13:58:51 -0500

    goimports

--- serve_https.go
+++ serve_https.go
@@ -1,5 +1,7 @@
 package web
 
+import "net/http"
+
 func ServeHTTPS(h http.Handler, acmeConfig ACMEConfig) error {
 	server := acmeConfig.HTTPSServer(h, acmeConfig.CertManager())
 	return server.ListenAndServeTLS("", "")
